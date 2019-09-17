082937451f429a0e13c430f669495b20f6c673dc
Ricardo Calixto <rcalixtos@gmail.com>
2019-09-16 21:54:58 -0300

    Uso do react-router-dom para lidar com paginação no front.

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/Repository" component={Repository} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
